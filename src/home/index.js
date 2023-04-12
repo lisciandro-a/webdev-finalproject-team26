52a27f35d9fd55d736ddc50ab88ef94c13e54118
Allison Lisciandro <80727184+lisciandro-a@users.noreply.github.com>
2023-04-12 19:22:54 -0400

    Add links to login and register from home page

--- src/home/index.js
+++ src/home/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
         <div>
-            Home page
+            <div> Home page </div>
+            <div>
+                <Link to="/login">Login</Link> or <Link to="/register">Register</Link>
+            </div>
+            
         </div>
     );
 }
